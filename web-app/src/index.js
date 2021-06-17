2aca5cc59c3d7e49218c22b01753bf09142952b8
ggulgulia <gajendra.gulgulia@gmail.com>
2021-06-17 10:45:30 +0200

    GGU-004: created a dumb footer

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -3,12 +3,29 @@ import ReactDom from 'react-dom';
 import './index.css'
 import logo from './images/GG_Pic.png';
 
-const App = () =>{
-  return (<section id='header'>
+const Footer = () =>{
+  return (<section id="footer">
+            <div>
+              <h1>This is footer</h1>
+            </div>
+        </section>);
+}
+
+const Header = () =>{
+  return (
+    <section id='header'>
           <div>
             <img src={logo}/>
           </div>
-        </section>);
+        </section>
+  );
+}
+const App = () =>{
+  return (<div>
+          <Header/>
+          <Footer/>
+          </div>
+      );
 }
 
 ReactDom.render(<App/>, document.getElementById('root'));
